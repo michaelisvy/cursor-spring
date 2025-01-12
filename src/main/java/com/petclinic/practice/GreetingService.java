3edd072a215575ca06bc6473235a00ae400d4417
Michael Isvy <michael.isvy@gmail.com>
2025-01-12 11:22:09 +0800

    added example with Service and Repository

--- src/main/java/com/petclinic/practice/GreetingService.java
+++ src/main/java/com/petclinic/practice/GreetingService.java
@@ -1,13 +1,8 @@
 package com.petclinic.practice;
 
 public class GreetingService {
-    private final String greeting;
-
-    public GreetingService(String greeting) {
-        this.greeting = greeting;
-    }
 
     public String sayHi() {
-        return greeting + " John";
+        return "Hello John";
     }
 }
--- src/test/java/com/petclinic/practice/GreetingServiceTest.java
+++ src/test/java/com/petclinic/practice/GreetingServiceTest.java
@@ -5,9 +5,10 @@ import org.junit.jupiter.api.Test;
 
 public class GreetingServiceTest {
 
+    private final GreetingService greetingService = new GreetingService();
+
     @Test
     public void shouldGreetSuccessfully() {
-        var greetingService = new GreetingService("Hello");
         assertThat(greetingService.sayHi()).isEqualTo("Hello John");
     }
 }
