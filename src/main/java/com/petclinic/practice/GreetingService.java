3a880865b54248b547cc51e117b7709e10c043e2
Michael Isvy <michael.isvy@gmail.com>
2025-01-12 08:36:57 +0800

    reinitialise project

--- src/main/java/com/petclinic/practice/GreetingService.java
+++ src/main/java/com/petclinic/practice/GreetingService.java
@@ -1,11 +1,7 @@
 package com.petclinic.practice;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public class GreetingService {
-    
-    public String sayHi(String name) {
-        return "Hello " + name;
+    public String sayHi() {
+        return "Hello John";
     }
 }
--- src/test/java/com/petclinic/practice/GreetingServiceTest.java
+++ src/test/java/com/petclinic/practice/GreetingServiceTest.java
@@ -1,20 +1,14 @@
 package com.petclinic.practice;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@TestPropertySource(properties = "greeting.name=John")
+import static org.assertj.core.api.Assertions.*;
+
 public class GreetingServiceTest {
-
-    @Autowired
-    private GreetingService greetingService;
 
     @Test
     public void shouldGreetSuccessfully() {
-        assertThat(greetingService.sayHi("John")).isEqualTo("Hello John");
+        var greetingService = new GreetingService();
+        assertThat(greetingService.sayHi()).isEqualTo("Hello John");
     }
 }
