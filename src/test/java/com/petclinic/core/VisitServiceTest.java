e71d809ab96a3d9fb68b62a6decc6da384d0cf99
Michael Isvy <michael.isvy@gmail.com>
2025-01-12 11:25:57 +0800

    removed unused method

--- src/test/java/com/petclinic/core/VisitServiceTest.java
+++ src/test/java/com/petclinic/core/VisitServiceTest.java
@@ -1,26 +1,17 @@
 package com.petclinic.core;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-
-import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 public class VisitServiceTest {
     @Autowired
     private VisitService visitService;
-    @Autowired
-    private ApplicationContext applicationContext;
 
     @Test
     public void shouldFindVisit() {
         assertThat(this.visitService.findByReferenceNumber("V01-23").getReferenceNumber()).isEqualTo("V01-23");
-    }
-
-    @Test
-    public void shouldDisplayBeanNumber() {
-        System.out.println(this.applicationContext.getBeanDefinitionCount());
     }
 }
